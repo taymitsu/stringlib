d4ac08935e2ae6e664c45c4452fff3f6f9640c28
taymitsu <tmitsumori96@gmail.com>
2022-05-23 16:07:54 -0700

    Challenge one complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello Universe')
+//CHALLENGE 1
+
+//Capitalize Function capitalize()
+//makes FIRST character of given string Uppercase 
+
+const str = 'henlo friendlo'
+
+//console.log( str[0] )
+//console.log( str.length )
+//console.log( str.toUpperCase() )
+//console.log( str.slice(3) )
+//takes slice of string starting at given [index]
+
+function capitalize(str) {
+  const first = str[0].toUpperCase()
+  const rest = str.slice(1)
+  return first + rest 
+}
+
+console.log( capitalize(str) )
+console.log( capitalize('mitsumori') )
+console.log( capitalize('my monstera is happy') )
