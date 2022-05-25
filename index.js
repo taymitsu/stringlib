d22d45dbba9d84d659f250096e871f62a91fb4ed
taymitsu <tmitsumori96@gmail.com>
2022-05-25 15:31:04 -0700

    Challenge 1 strategy 2 added

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+///
+//https://replit.com/@taymitsu/stringlib#index.js
+//
+
+////////////////////////////////////////////////////////////////////
+
 //CHALLENGE 1
 
 //Capitalize Function capitalize()
 //makes FIRST character of given string Uppercase 
+
+////////////////////////////////////////////////////////////////////
 
 const str = 'henlo friendlo'
 
@@ -20,3 +28,41 @@ function capitalize(str) {
 console.log( capitalize(str) )
 console.log( capitalize('mitsumori') )
 console.log( capitalize('my monstera is happy') )
+
+//CHALLENGE 1, Strategy 2
+//console.log( str.split(' ') )
+//splits on space character 
+
+const str = 'henlo friendlo'
+
+function capitalize(str) {
+  const arr = str.split('')
+  arr[0] = arr[0].toUpperCase()
+  return( arr.join('') ) 
+}
+
+console.log( capitalize(str) )
+console.log( capitalize('i love my apartment') )
+console.log( capitalize('my dogs are mo betta') )
+
+////////////////////////////////////////////////////////////////////
+
+//CHALLENGE 2
+
+//Capitalize ALL characters, allCaps()
+//makes ALL first character of given string Uppercase ex: First Character Of Entire String
+
+const str = 'henlo friendlo'
+
+function capitalizeWords(str) {
+  const words = str.split(' ')
+  const upperwords = words.map( (word) => {} )
+}
+
+function allCaps(str) {
+  return str.toUpperCase()
+}
+
+console.log(allCaps(str))
+
+////////////////////////////////////////////////////////////////////
