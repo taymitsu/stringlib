a81387b54a4e47b50605998505b6efd08e91bc59
taymitsu <tmitsumori96@gmail.com>
2022-05-26 20:30:25 -0700

    Challenge 3 and formatting implemented

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 
 ////////////////////////////////////////////////////////////////////
 
-const str = 'henlo friendlo'
+//const str = 'henlo friendlo'
 
 //console.log( str[0] )
 //console.log( str.length )
@@ -25,15 +25,9 @@ function capitalize(str) {
   return first + rest 
 }
 
-console.log( capitalize(str) )
-console.log( capitalize('mitsumori') )
-console.log( capitalize('my monstera is happy') )
-
 //CHALLENGE 1, Strategy 2
 //console.log( str.split(' ') )
 //splits on space character 
-
-const str = 'henlo friendlo'
 
 function capitalize(str) {
   const arr = str.split('')
@@ -41,28 +35,44 @@ function capitalize(str) {
   return( arr.join('') ) 
 }
 
-console.log( capitalize(str) )
-console.log( capitalize('i love my apartment') )
-console.log( capitalize('my dogs are mo betta') )
+//console.log( capitalize(str) )
+//console.log( capitalize('i love my apartment') )
+//console.log( capitalize('my dogs are mo betta') )
 
 ////////////////////////////////////////////////////////////////////
 
 //CHALLENGE 2
-
 //Capitalize ALL characters, allCaps()
-//makes ALL first character of given string Uppercase ex: First Character Of Entire String
 
-const str = 'henlo friendlo'
-
-function capitalizeWords(str) {
-  const words = str.split(' ')
-  const upperwords = words.map( (word) => {} )
-}
 
 function allCaps(str) {
   return str.toUpperCase()
 }
 
-console.log(allCaps(str))
+//console.log(allCaps(str))
+//console.log( capitalizeWords(str) )
+//console.log( capitalizeWords('i love my doge') )
+//console.log( capitalizeWords('i am excited for may') )
+
 
 ////////////////////////////////////////////////////////////////////
+
+//CHALLENGE 3
+//makes ALL first character of given string Uppercase ex: First Character Of Entire String
+
+function capitalizeWords(str) {
+  const words = str.split(' ')
+  const upperWords = words.map( (word) => capitalize(word) )
+  return upperWords.join(' ')
+}
+
+//CHALLENGE 4
+//removeExtraSpaces()
+
+console.log( capitalize('henlo friendlo') )
+console.log( capitalize('mitsumori') )
+console.log( capitalize('my monstera is happy') )
+
+console.log( capitalizeWords('hi earth') )
+console.log( capitalizeWords('henlo friendlo how are you') )
+console.log( capitalizeWords('my dogs are mo betta than yours') )
